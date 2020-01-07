1ae28c6c80545bb08333255666a998a7b2c7f540
Damien Carrier <damien.carrier@mines-ales.org>
2020-01-07 18:09:34 +0100

    Develop

--- netshowIp/netshowIp.c
+++ netshowIp/netshowIp.c
@@ -10,43 +10,69 @@
 #include <ifaddrs.h>
 #include <string.h>
 
+static int bitCount(uint32_t i) {
+    int count = 0;
+
+    while (i > 0) {
+        if (1 == i) {
+            count++;
+        }
+
+        i >>= 1;
+    }
+
+    return count;
+}
+
 int nethowIp(char *ifname[])
 {
     struct ifaddrs *list;
     int res = getifaddrs(&list);
     char host[NI_MAXHOST];
+    char addr[50];
+    char mask[50];
 
     if (0 == res) {
-        printf("Vous voulez des informations sur : %s\n", ifname[1]);
+        printf("Vous voulez des informations sur : %s\n\n", ifname[1]);
 
         while (list) {
             if (!strcmp(list->ifa_name, ifname[1])) {
-                struct sockaddr *sockaddr = list->ifa_addr;
+                if (AF_INET == list->ifa_addr->sa_family) {
+                    struct sockaddr_in *in = (struct sockaddr_in *) list->ifa_addr;
+                    inet_ntop(AF_INET, &in->sin_addr, addr, sizeof(addr));
 
-                if (AF_INET == sockaddr->sa_family || AF_INET6 == sockaddr->sa_family) {
-                    int s = getnameinfo(
-                            sockaddr,
-                            (AF_INET == sockaddr->sa_family) ? sizeof(struct sockaddr_in) : sizeof(struct sockaddr_in6),
-                            host,
-                            NI_MAXHOST,
-                            NULL,
-                            0,
-                            NI_NUMERICHOST
-                     );
+                    struct sockaddr_in *mask4 = (struct sockaddr_in *) list->ifa_netmask;
+                    inet_ntop(AF_INET, &mask4->sin_addr, mask, sizeof(addr));
 
-                    if (0 != s) {
-                        printf("getnameinfo failed\n");
-                    } else {
-                        switch (sockaddr->sa_family) {
-                            case AF_INET:
-                                printf("adresse IPv4: %s\n", host);
-                                break;
-                            case AF_INET6:
-                                printf("adresse IPv6: %s\n", host);
-                                break;
-                        }
+                    int n;
+                    inet_pton(AF_INET, mask, &n);
+                    int i = 0;
+
+                    while (n > 0) {
+                        n = n >> 1;
+                        i++;
                     }
+
+                    printf("\t- adresse IPv4: %s\n", addr);
+                    printf("\t- masque réseau (IPv4) : /%d\n", i);
+                } else if (AF_INET6 == list->ifa_addr->sa_family) {
+                    const void *addr = &((struct sockaddr_in6 *) list->ifa_addr)->sin6_addr;
+                    char ipstrv6[INET6_ADDRSTRLEN];
+                    inet_ntop(AF_INET6, addr, ipstrv6, sizeof(ipstrv6));
+
+                    const void *netaddr = &((struct sockaddr_in6 *) list->ifa_netmask)->sin6_addr;
+                    struct in6_addr netmaskv6 = (((struct sockaddr_in6 *) list->ifa_netmask)->sin6_addr);
+                    int cidrv6 = 0;
+
+                    for (int byte = 0; byte < 16; byte++) {
+                        int temp = bitCount(netmaskv6.s6_addr[byte]);
+                        cidrv6 = cidrv6 + temp;
+                    }
+
+                    printf("\t- adresse IPv6: %p\n", ipstrv6);
+                    printf("\t- masque réseau (IPv4) : /%d\n", cidrv6);
                 }
+
             }
 
             list = list->ifa_next;
